9943a378140c868c7cb826dbe56a8f0d3b06c282
mikayBuk <mmb19@rice.edu>
2023-05-22 07:23:42 -0500

    Fixed Errors, Now Displaying Constantly Updated Graph

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -42,11 +42,11 @@ class Graph extends Component<IProps, {}> {
     };
 
     elem.setAttribute("view", "y_line");
-    elem.setAttribute("column-pivots", "['stock']");
-    elem.setAttribute("row-pivots", "['timestamp']");
-    elem.setAttribute("columns", "['top_ask_price']");
+    elem.setAttribute("column-pivots", '["stock"]');
+    elem.setAttribute("row-pivots", '["timestamp"]');
+    elem.setAttribute("columns", '["top_ask_price"]');
     elem.setAttribute("aggregates",
-     "{'stock':'distinct count', 'top_ask_price':'avg', 'top_bid_price':'avg', 'timestamp':'distinct count'}");
+     '{"stock":"distinct count", "top_ask_price":"avg", "top_bid_price":"avg", "timestamp":"distinct count"}');
 
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
